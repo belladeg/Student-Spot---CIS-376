4921c526afdde289576cfe03268219997982bec4
belladeg <belladeg@umich.edu>
2026-08-13 19:24:35 -0400

    Added MainViewModel.kt, setup LoginFragment

--- app/src/main/java/com/group6/studentspot/LoginFragment.java
+++ app/src/main/java/com/group6/studentspot/LoginFragment.java
@@ -8,6 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.group6.studentspot.databinding.FragmentLoginBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link LoginFragment#newInstance} factory method to
@@ -23,6 +28,9 @@ public class LoginFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private MainViewModel viewModel;
+    private FragmentLoginBinding binding;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -56,9 +64,15 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
---
+++ app/src/main/java/com/group6/studentspot/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.group6.studentspot
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * A ViewModel that handles LiveData manipulation to be used by the observing fragments.
+ *
+ * Handles Google Firebase integration.
+ */
+class MainViewModel : ViewModel() {
+
+}
